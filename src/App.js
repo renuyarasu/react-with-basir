3e5e23e33fcc370da6c4607834f575a15db09766
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-17 22:58:09 +0530

    basir: Intro To JSX+img

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  
   function formateName(username) {
     return username.toUpperCase();
   }
@@ -10,16 +9,22 @@ function App() {
   function greetings(username) {
     if (username) {
       return <h1>Hello {formateName(username)}</h1>;
-    } 
-    else {
+    } else {
       return <h1>Hello Stranger!</h1>;
     }
   }
 
   const name = "Renuka Prsad";
-  const element = <h1>Hello {formateName(name)}</h1>;
+  const imgPath = "./logo512.png";
+  const img = <img src={imgPath} width="200" height="200" />;
+  const element = (
+    <div>
+      <h1>Hello {name}</h1>;
+      {img}
+    </div>
+  );
 
-  return greetings('Gnapika');
+  return element;
 }
 
 export default App;
